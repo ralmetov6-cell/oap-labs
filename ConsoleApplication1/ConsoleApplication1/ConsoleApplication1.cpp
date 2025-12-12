ae9f5f5362b4565db6139b3f9857c1d7f14f8b8f
Студент <marisendikaeva@mail.ru>
2025-12-12 09:19:24 +0300

    Первый коммит. Добавлен проект ОАиП

---
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,28 @@
+﻿#include <stdio.h>
+#include <math.h>
+#include <locale.h>
+
+int main(void) {
+    setlocale(LC_ALL, "Russian");
+
+    int N;
+    double S = 0.0;
+
+    printf("Введите целое число N (> 0): ");
+    if (scanf_s("%d", &N) != 1 || N <= 0) {
+        printf("Ошибка ввода. N должно быть целым числом > 0.\n");
+        return 1;
+    }
+
+    for (int i = 1; i <= N; i++) {
+        S += 1.0 / i;
+    }
+
+    printf("Сумма ряда = %.4f\n", S);
+
+    printf("Нажмите Enter для выхода...");
+    getchar();
+    getchar();
+
+    return 0;
+}
